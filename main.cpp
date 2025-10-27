e58148908bb6d84434558625f3653442a809f03d
Sarah Davis <sdavis2@nic.edu>
2025-10-27 10:44:20 -0700

    Added say function

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
 using namespace std;
+void say(string s);
 
 int main()
 {
     cout << "Greeting...\n";     
     cout << "Hello World!\n";
     return 0;
+}
+
+void say(string s)
+{
+    cout << s << endl;
 }
